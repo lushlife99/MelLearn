733cf383554f43349633006eb7dbb92fa8c49c03
hyomin <leehm123456@gmail.com>
2024-04-07 20:45:34 +0900

    음악 재생 UI

---
+++ frontend/src/musichome/Lyric.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Lyric() {
+  return <div></div>;
+}
+
+export default Lyric;
--- frontend/src/musichome/PlayMusic.tsx
+++ frontend/src/musichome/PlayMusic.tsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { axiosSpotify, axiosSpotifyScraper } from "../api";
 import { useLocation, useNavigate } from "react-router-dom";
-import { FaPlay, FaPause, FaStepBackward, FaStepForward } from "react-icons/fa";
+import {
+  FaPlay,
+  FaPause,
+  FaStepBackward,
+  FaStepForward,
+  FaPlayCircle,
+  FaPauseCircle,
+} from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
+import { IoIosArrowRoundBack, IoIosArrowUp } from "react-icons/io";
+import { LuPencilLine } from "react-icons/lu";
+import { FaMicrophoneLines } from "react-icons/fa6";
 
 export interface CurrentTimeData {
   progress_ms: number;
@@ -26,17 +36,25 @@ function PlayMusic() {
     const playbackState = await axiosSpotify.get("/me/player"); //현재 재생 위치 확인
     const res2 = await axiosSpotify.get("/me/player/currently-playing");
 
-    const existingPlaying = res2.data.item.id;
-    //새로운 음악 재생시 새로운 재생
-    //const progress_ms = 0;
-    const progress_ms =
-      track.id === existingPlaying ? playbackState.data.progress_ms : 0;
+    let progress_ms = 0;
+    if (res2.data.item === undefined) {
+      console.log("success");
+      progress_ms = 0;
+    } else {
+      if (track.id === res2.data.item.id) {
+        progress_ms = playbackState.data.progress_ms;
+      } else {
+        progress_ms = 0;
+      }
+    }
+
     const res = await axiosSpotify.put("/me/player/play", {
       uris: ["spotify:track:" + track.id],
       position_ms: progress_ms,
     });
 
     if (res.status === 202) {
+      startProgressBar();
       setIsPlaying(true);
     }
   };
@@ -47,6 +65,7 @@ function PlayMusic() {
 
     if (res.status === 202) {
       setIsPlaying(false);
+      stopProgressBar();
     }
   };
 
@@ -64,60 +83,121 @@ function PlayMusic() {
   const goBack = () => {
     navigate(-1); //뒤로가기
   };
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [intervalId, setIntervalId] =
+    useState<ReturnType<typeof setInterval>>();
+  const startProgressBar = () => {
+    const interval = setInterval(() => {
+      setCurrentTime((prevTime) => prevTime + 1000);
+      if (currentTime === duration) {
+        clearInterval(intervalId);
+      }
+    }, 1000);
+    setIntervalId(interval);
+  };
+  const [duration, setDuration] = useState<number>(0);
+  useEffect(() => {
+    setDuration(track.duration_ms);
+  }, []);
+
+  const stopProgressBar = () => {
+    clearInterval(intervalId);
+  };
+  const progressPercentage = (currentTime / duration) * 100;
 
   return (
     <div className="bg-[#9bd1e5] flex flex-row justify-center w-full h-screen">
-      <div className="relative bg-[black] overflow-hidden w-full max-w-[450px] h-screen  flex flex-col px-3">
-        <span className="text-[white] text-center text-3xl font-bold mt-12">
-          {track.name}
-        </span>
-        <div className="flex items-center justify-center mt-16 ">
+      <div className="relative bg-[black] overflow-hidden w-full max-w-[450px] h-screen  flex flex-col px-5">
+        <IoIosArrowRoundBack
+          onClick={goBack}
+          className="fill-[white] w-10 h-10 mt-8 hover:opacity-60"
+        />
+        {/* 앪범 커버 */}
+        <div className="flex items-center justify-center mt-4 ">
           <img
             src={track.album.images[0].url}
             alt="Album Cover"
-            className="rounded-lg w-80 h-80"
+            className="rounded-lg w-84 h-84"
           />
         </div>
-        <div className="border border-white">
+
+        {/* 트랙 명 */}
+        <span className="text-[white]  text-3xl font-bold mt-4 mb-1">
+          {track.name}
+        </span>
+
+        {/* 아티스트 이름*/}
+        <div>
           {track.artists.map((artist: any, index: number) => (
-            <span
-              key={index}
-              className="text-[white] text-left text-3xl font-bold"
-            >
+            <span key={index} className="text-[#B3B3B3] text-left text-lg ">
               {artist.name}
             </span>
           ))}
         </div>
-        <div className="flex justify-around mt-72">
+
+        {/* progress Bar */}
+        <div className="w-full h-2 mt-12 bg-gray-200 rounded-full">
+          <div
+            className={`h-full bg-green-500 rounded-full transition-all duration-500 ease-in-out`}
+            style={{ width: `${progressPercentage}%` }}
+          ></div>
+        </div>
+
+        {/** 재생 시간*/}
+        <div className="flex justify-between mt-1">
+          <span className="text-[#B3B3B3]">
+            {Math.floor(currentTime / 1000 / 60)}:
+            {Math.floor((currentTime / 1000) % 60) < 10
+              ? `0${Math.floor((currentTime / 1000) % 60)}`
+              : Math.floor((currentTime / 1000) % 60)}
+          </span>
+          <span className=" text-[#B3B3B3]">
+            {Math.floor(track.duration_ms / 1000 / 60)}:
+            {Math.floor((track.duration_ms / 1000) % 60) < 10
+              ? `0${Math.floor((track.duration_ms / 1000) % 60)}`
+              : Math.floor((track.duration_ms / 1000) % 60)}
+          </span>
+        </div>
+        {/* 버튼 div */}
+        <div className="flex justify-between mt-12">
+          <button className="bg-[#D3D3D3] rounded-2xl h-9 w-28 flex items-center justify-center hover:opacity-60">
+            <LuPencilLine />
+            문제풀기
+          </button>
+          <div className="flex flex-col items-center">
+            <div className=" flex flex-col justify-center items-center text-[#d3d3d3] hover:opacity-60">
+              <IoIosArrowUp />
+              <span>가사</span>
+            </div>
+          </div>
+          <button className="bg-[#D3D3D3] rounded-2xl h-9 w-28 flex items-center justify-center hover:opacity-60">
+            <FaMicrophoneLines />
+            따라부르기
+          </button>
+        </div>
+
+        {/*아이콘 */}
+        <div className="flex items-center justify-around mt-24">
           <FaStepBackward
             onClick={playPrevious}
-            className="fill-[white] w-10 h-10 hover:opacity-60"
+            className="fill-[white] w-8 h-8 hover:opacity-60"
           />
           {!isPlaying ? (
-            <FaPlay
-              className="fill-[white] w-10 h-10 hover:opacity-60"
+            <FaPlayCircle
+              className="fill-[white] w-16 h-16 hover:opacity-60"
               onClick={play}
             />
           ) : (
-            <FaPause
-              className="fill-[white] w-10 h-10 hover:opacity-60"
+            <FaPauseCircle
+              className="fill-[white] w-16 h-16 hover:opacity-60"
               onClick={pause}
             />
           )}
           <FaStepForward
             onClick={playNext}
-            className="fill-[white] w-10 h-10 hover:opacity-60"
+            className="fill-[white] w-8 h-8 hover:opacity-60"
           />
         </div>
-        <span className="text-[white] text-xl">
-          {Math.floor(track.duration_ms / 1000 / 60)}:
-          {Math.floor((track.duration_ms / 1000) % 60) < 10
-            ? `0${Math.floor((track.duration_ms / 1000) % 60)}`
-            : Math.floor((track.duration_ms / 1000) % 60)}
-        </span>
-        <span className="text-[white] text-xl" onClick={goBack}>
-          뒤로가기
-        </span>
       </div>
     </div>
   );
