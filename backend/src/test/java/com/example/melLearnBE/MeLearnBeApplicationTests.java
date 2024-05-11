746e50d6a1320ad86ec836d2c14480e6d9d5a125
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-05-11 21:12:46 +0900

    Fix History Querydsl에서 ElementCollection은 fetch를 지원하지 않는 문제 수정

--- backend/src/test/java/com/example/melLearnBE/MeLearnBeApplicationTests.java
+++ backend/src/test/java/com/example/melLearnBE/MeLearnBeApplicationTests.java
@@ -1,16 +1,14 @@
 package com.example.melLearnBE;
 
 import com.example.melLearnBE.enums.LearningLevel;
-import com.example.melLearnBE.model.ListeningQuiz;
-import com.example.melLearnBE.model.ListeningSubmit;
-import com.example.melLearnBE.model.Member;
-import com.example.melLearnBE.repository.ListeningQuizRepository;
-import com.example.melLearnBE.repository.ListeningSubmitRepository;
-import com.example.melLearnBE.repository.MemberRepository;
+import com.example.melLearnBE.enums.QuizType;
+import com.example.melLearnBE.model.*;
+import com.example.melLearnBE.repository.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
@@ -22,15 +20,21 @@ class MeLearnBeApplicationTests {
     private ListeningSubmitRepository listeningSubmitRepository;
     @Autowired
     private ListeningQuizRepository listeningQuizRepository;
+    @Autowired
+    private SpeakingSubmitRepository speakingSubmitRepository;
+    @Autowired
+    private QuizListRepository quizListRepository;
+    @Autowired
+    private QuizSubmitRepository quizSubmitRepository;
 
     @Autowired
     private MemberRepository memberRepository;
 
     @Test
     void contextLoads() {
+        Member member = memberRepository.findByMemberId("a1").get();
 
-        for(int i = 0; i < 21; i++) {
-            Member member = memberRepository.findByMemberId("a1").get();
+        for (int i = 0; i < 21; i++) {
 
             ListeningQuiz listeningQuiz = ListeningQuiz.builder()
                     .blankedText("asdfasdfasdf")
@@ -40,6 +44,22 @@ class MeLearnBeApplicationTests {
                     .build();
             listeningQuiz = listeningQuizRepository.save(listeningQuiz);
 
+            QuizList vocaQuizList = QuizList.builder()
+                    .quizType(QuizType.VOCABULARY)
+                    .quizzes(new ArrayList<>())
+                    .level(LearningLevel.Beginner)
+                    .musicId(UUID.randomUUID().toString())
+                    .build();
+            vocaQuizList = quizListRepository.save(vocaQuizList);
+
+            QuizList readingQuizList = QuizList.builder()
+                    .quizType(QuizType.READING)
+                    .quizzes(new ArrayList<>())
+                    .level(LearningLevel.Beginner)
+                    .musicId(UUID.randomUUID().toString())
+                    .build();
+
+            readingQuizList = quizListRepository.save(readingQuizList);
 
             ListeningSubmit listeningSubmit = ListeningSubmit.builder()
                     .score(100)
@@ -48,10 +68,36 @@ class MeLearnBeApplicationTests {
                     .level(LearningLevel.Beginner)
                     .submitAnswerList(List.of("a", "b", "c"))
                     .build();
-            listeningSubmit = listeningSubmitRepository.save(listeningSubmit);
+            listeningSubmitRepository.save(listeningSubmit);
+
+            SpeakingSubmit speakingSubmit = SpeakingSubmit.builder()
+                    .submit("asdf")
+                    .musicId(UUID.randomUUID().toString())
+                    .score(100.0)
+                    .member(member)
+                    .build();
+            speakingSubmitRepository.save(speakingSubmit);
+
+            QuizSubmit vocaQuizSubmit = QuizSubmit.builder()
+                    .quizList(vocaQuizList)
+                    .submitAnswerList(List.of(1, 2, 3, 4, 5))
+                    .score(100.0)
+                    .member(member)
+                    .build();
+            quizSubmitRepository.save(vocaQuizSubmit);
+
+            QuizSubmit readingQuizSubmit = QuizSubmit.builder()
+                    .quizList(readingQuizList)
+                    .submitAnswerList(List.of(1, 2, 3, 4, 5))
+                    .score(100.0)
+                    .member(member)
+                    .build();
+            quizSubmitRepository.save(readingQuizSubmit);
+
+
 
         }
-
     }
+
 
 }
