4b4cb410b90e02bf23b84a01c5125d07ffe0d67f
lushlife99 <wjdcks282828@gmail.com>
2025-03-31 00:55:45 +0900

    refactor : SubmitJpaRepository 쿼리 단계에서 DTO 변환

--- backend/src/main/java/com/example/melLearnBE/repository/querydsl/SubmitJpaRepository.java
+++ backend/src/main/java/com/example/melLearnBE/repository/querydsl/SubmitJpaRepository.java
@@ -3,20 +3,18 @@ package com.example.melLearnBE.repository.querydsl;
 import com.example.melLearnBE.dto.model.*;
 import com.example.melLearnBE.enums.QuizType;
 import com.example.melLearnBE.model.*;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.ListPath;
-import com.querydsl.core.types.dsl.StringPath;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.example.melLearnBE.model.QListeningQuiz.listeningQuiz;
 import static com.example.melLearnBE.model.QListeningSubmit.listeningSubmit;
+import static com.example.melLearnBE.model.QQuizList.quizList;
 import static com.example.melLearnBE.model.QQuizSubmit.quizSubmit;
 import static com.example.melLearnBE.model.QSpeakingSubmit.speakingSubmit;
 
@@ -36,46 +34,59 @@ public class SubmitJpaRepository {
      *
      * 일단 지금은 트랜잭션을 사용해 Lazy-Fetch로 값을 불러오고, 나중에 수정하기.
      */
-    @Transactional
+    @Transactional(readOnly = true)
     public Page<QuizSubmitDto> findSubmitWithPaging(long memberId, QuizType quizType, int pageNumber, int pageSize) {
-
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
 
-        List<QuizSubmit> results = queryFactory
-                .selectFrom(quizSubmit)
-                .where(quizSubmit.member.id.eq(memberId))
-                .where(quizSubmit.quizList.quizType.eq(quizType))
+        List<QuizSubmitDto> results = queryFactory
+                .select(Projections.constructor(QuizSubmitDto.class,
+                        quizSubmit.id,
+                        Projections.constructor(QuizListDto.class,
+                                quizSubmit.quizList.id,
+                                quizSubmit.quizList.quizType,
+                                quizSubmit.quizList.level,
+                                quizSubmit.quizList.musicId
+                        ),
+                        quizSubmit.submitAnswerList,
+                        quizSubmit.score,
+                        quizSubmit.createdTime
+                ))
+                .from(quizSubmit)
+                .join(quizSubmit.quizList, quizList).fetchJoin()
+                .where(quizSubmit.member.id.eq(memberId),
+                        quizSubmit.quizList.quizType.eq(quizType))
                 .orderBy(quizSubmit.createdTime.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
-
         long total = queryFactory
                 .selectFrom(quizSubmit)
-                .where(quizSubmit.member.id.eq(memberId))
-                .where(quizSubmit.quizList.quizType.eq(quizType))
+                .where(quizSubmit.member.id.eq(memberId),
+                        quizSubmit.quizList.quizType.eq(quizType))
                 .fetchCount();
 
-        List<QuizSubmitDto> result = results.stream()
-                .map(submit -> new QuizSubmitDto(
-                        submit.getId(),
-                        new QuizListDto(submit.getQuizList()),
-                        submit.getSubmitAnswerList(),
-                        submit.getScore(),
-                        submit.getCreatedTime()))
-                .collect(Collectors.toList());
-
-        return new PageImpl<>(result, pageable, total);
-
+        return new PageImpl<>(results, pageable, total);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public Page<ListeningSubmitDto> findListeningSubmitWithPaging(long memberId, int pageNumber, int pageSize) {
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
 
-        List<ListeningSubmit> results = queryFactory
-                .selectFrom(listeningSubmit)
+        List<ListeningSubmitDto> results = queryFactory
+                .select(Projections.constructor(ListeningSubmitDto.class,
+                        listeningSubmit.id,
+                        Projections.constructor(ListeningQuizDto.class,
+                                listeningSubmit.listeningQuiz.id,
+                                listeningSubmit.listeningQuiz.musicId,
+                                listeningSubmit.listeningQuiz.level
+                        ),
+                        listeningSubmit.level,
+                        listeningSubmit.submitAnswerList,
+                        listeningSubmit.score,
+                        listeningSubmit.createdTime
+                ))
+                .from(listeningSubmit)
                 .join(listeningSubmit.listeningQuiz, listeningQuiz).fetchJoin()
                 .where(listeningSubmit.member.id.eq(memberId))
                 .orderBy(listeningSubmit.createdTime.desc())
@@ -84,32 +95,27 @@ public class SubmitJpaRepository {
                 .fetch();
 
         long total = queryFactory
-                .select(listeningSubmit)
-                .from(listeningSubmit)
-                .join(listeningSubmit.listeningQuiz)
+                .selectFrom(listeningSubmit)
                 .where(listeningSubmit.member.id.eq(memberId))
                 .fetchCount();
 
-
-        List<ListeningSubmitDto> result = results.stream()
-                .map(submit -> new ListeningSubmitDto(
-                        submit.getId(),
-                        new ListeningQuizDto(submit.getListeningQuiz()),
-                        submit.getLevel(),
-                        submit.getSubmitAnswerList(),
-                        submit.getScore(),
-                        submit.getCreatedTime()))
-                .collect(Collectors.toList());
-        return new PageImpl<>(result, pageable, total);
+        return new PageImpl<>(results, pageable, total);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public Page<SpeakingSubmitDto> findSpeakingSubmitWithPaging(long memberId, int pageNumber, int pageSize) {
-
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
 
-        List<SpeakingSubmit> results = queryFactory
-                .selectFrom(speakingSubmit)
+        List<SpeakingSubmitDto> results = queryFactory
+                .select(Projections.constructor(SpeakingSubmitDto.class,
+                        speakingSubmit.id,
+                        speakingSubmit.musicId,
+                        speakingSubmit.submit,
+                        speakingSubmit.markedText,
+                        speakingSubmit.score,
+                        speakingSubmit.createdTime
+                ))
+                .from(speakingSubmit)
                 .where(speakingSubmit.member.id.eq(memberId))
                 .orderBy(speakingSubmit.createdTime.desc())
                 .offset(pageable.getOffset())
@@ -121,18 +127,6 @@ public class SubmitJpaRepository {
                 .where(speakingSubmit.member.id.eq(memberId))
                 .fetchCount();
 
-        List<SpeakingSubmitDto> result = results.stream()
-                .map(submit -> new SpeakingSubmitDto(
-                        submit.getId(),
-                        submit.getMusicId(),
-                        submit.getSubmit(),
-                        submit.getMarkedText(),
-                        submit.getScore(),
-                        submit.getCreatedTime()))
-                .collect(Collectors.toList());
-
-        return new PageImpl<>(result, pageable, total);
+        return new PageImpl<>(results, pageable, total);
     }
-
-
 }
